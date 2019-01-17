e93c4fdcb68f1610e980e5dd759b2af2d28b7a70
g.bailly <g.bailly@immanens.coms>
2019-01-17 11:23:32 +0100

    Mise a jour de la page login

--- src/views/frames/login/main.js
+++ src/views/frames/login/main.js
@@ -50,7 +50,7 @@ export default class Main extends superViews{
 		    card.setStyle("display", "flex");
 			card.setStyle("flexDirection", "column");
 			card.setStyle("justifyContent", "center");
-			card.setStyle("alignItems:", "center");
+			card.setStyle("alignItems", "center");
 
 
 
