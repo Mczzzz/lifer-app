35f5c1219ad770fcf9104b3149089f7f656e8a7d
g.bailly <g.bailly@immanens.coms>
2019-01-18 14:16:53 +0100

    ajout éeme logo

--- src/workers/sw.js
+++ src/workers/sw.js
@@ -10,7 +10,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
+/*  event.respondWith(
     caches.match(event.request)
-  );
+  );*/
 });
