470e3cf496b1dcf2a304b2e9b3bfa94d63d89368
g.bailly <g.bailly@immanens.coms>
2019-01-18 13:21:20 +0100

    Mise a jour install webp app

--- src/workers/sw.js
+++ src/workers/sw.js
@@ -2,11 +2,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/public/dist/lifer.js',
-        '/public/dist/sw.js',
-        '/public/index.html',
-        '/public/manifest.json',
-        'https://fonts.googleapis.com/css?family=Berkshire+Swash'
+        '/public/dist/lifer.js'
       ]);
     })
   );
