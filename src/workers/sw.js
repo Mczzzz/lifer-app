5c0ff7eaa5b0ec802087b994fad5959f4a30e8db
g.bailly <g.bailly@immanens.coms>
2019-01-18 16:57:06 +0100

    ajout 2eme logo

--- src/workers/sw.js
+++ src/workers/sw.js
@@ -9,8 +9,30 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
+/*self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
+  );
+});*/
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+
+      // TODO 4 - Add fetched files to the cache
+
+    }).catch(error => {
+
+      // TODO 6 - Respond with custom offline page
+
+    })
   );
 });
