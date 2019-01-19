350085d3067e3f94792a9d1901b747818ec210c8
Guillaume Bailly <g.bailly@immanens.com>
2019-01-19 09:02:53 +0100

    ajout du manifest html5

--- src/workers/sw.js
+++ src/workers/sw.js
@@ -16,7 +16,7 @@ self.addEventListener('install', function(event) {
 });*/
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch event for '+event.request.url);
+/*  console.log('Fetch event for '+event.request.url);
   event.respondWith(
     caches.match(event.request)
     .then(response => {
@@ -25,10 +25,10 @@ self.addEventListener('fetch', event => {
         return response;
       }
       console.log('Network request for ', event.request.url);
-      return fetch(event.request)
+      return fetch(event.request):
 
       // TODO 4 - Add fetched files to the cache
 
     })
-  );
+  );*/
 });
