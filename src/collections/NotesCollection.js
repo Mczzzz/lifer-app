7b7a698310db27c39ab4915796ae3ac6d5f8ac6e
Guillaume Bailly <g.bailly@immanens.com>
2019-01-20 06:12:02 +0100

    ajout du manifest html5

--- src/collections/NotesCollection.js
+++ src/collections/NotesCollection.js
@@ -495,8 +495,8 @@ export default class NotesCollection {
 			requestData.scope = "all";
 			//getallfrom server
 			//je set une value
-		let qry = "INSERT INTO Params (name, value) VALUES ('last_synchro',strftime('%Y-%m-%d %H:%M:%f', 'now'))";
-		this.webSQL.playQuery('cacheData',qry);
+	//	let qry = "INSERT INTO Params (name, value) VALUES ('last_synchro',strftime('%Y-%m-%d %H:%M:%f', 'now'))";
+	//	this.webSQL.playQuery('cacheData',qry);
 
 
 
