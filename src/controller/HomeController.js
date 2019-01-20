c52eccb7e6e5484d53ab13d1beb552cac1b97a19
Guillaume Bailly <g.bailly@immanens.com>
2019-01-20 08:05:24 +0100

    ajout du manifest html5

--- src/controller/HomeController.js
+++ src/controller/HomeController.js
@@ -1,5 +1,7 @@
 import { Lifer } from '../services/Lifer.js';
 
+import LoaderCollection from '../serivces/LoaderCollection.js';
+
 import Home from '../views/frames/home.js';
 
 export default class HomeController {
@@ -21,6 +23,7 @@ export default class HomeController {
 
 	init(){
 
+		this.UserCollection = new LoaderCollection('User');
 
 		this.initView();
 	}
@@ -34,13 +37,27 @@ export default class HomeController {
 		let home = new Home("Frame-Home");
 
 		//launch Login si pas connecté
-	//	console.log('dispatchEvent vers login');
-	//	let LinkEvent = new CustomEvent('changeRoute', {'detail' : {'frame' : 'Login'}});
+		this.UserCollection.isAuth(this,'_launchLogin');
+
+
+
+
+
 
 		
 	}
 
+	_launchLogin(isConnect){
 
+
+		if(!isConnect){
+
+			let LinkEvent = new CustomEvent('changeRoute', {'detail' : {'frame' : 'Login'}});
+			window.dispatchEvent(LinkEvent);
+
+		}
+
+	}
 
 
 
