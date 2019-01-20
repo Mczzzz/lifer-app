a832b0fe5ed48fb5e8f15989fa4c3d377b19b7de
Guillaume Bailly <g.bailly@immanens.com>
2019-01-20 09:55:16 +0100

    Mise a jour login test

--- src/collections/UserCollection.js
+++ src/collections/UserCollection.js
@@ -58,7 +58,7 @@ export default class UserCollection {
       console.log(datas);
       console.log(datas.rows);
       console.log(datas.rows.length);
-      console.log(datas.rows[0].is_auth);
+      if(datas.rows.length)  console.log(datas.rows[0].is_auth);
 
       if(datas.rows && datas.rows.length == 1 && datas.rows[0].is_auth){
 
