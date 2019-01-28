c32c094e3dc7033347d77b5e9ad9df8527511d08
g.bailly <g.bailly@immanens.coms>
2019-01-28 17:38:30 +0100

    Refacto services pour faire plus propre et sans oublie ou pas

--- src/workers/sw.js
+++ src/workers/sw.js
@@ -12,6 +12,9 @@ self.addEventListener('fetch', event => {
        console.log('nothing in cache to to network');
 
       fromNetwork(event.request,800)
+        .then(function(response)){
+          return response;
+        }
         .catch(function(reject){
   //      console.log('aiiiiieeeee');
         throw Error('ca a merdé ');
