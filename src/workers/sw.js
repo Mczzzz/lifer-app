186e0891b7d6528b73c6cd0ddbb06470325d572a
Guillaume Bailly <g.bailly@immanens.com>
2019-01-28 22:27:57 +0100

    Metoyage du service worker

--- src/workers/sw.js
+++ src/workers/sw.js
@@ -66,7 +66,7 @@ function fromNetwork(request, timeout) {
     console.log(timeout);*/
  //   let timeoutId = setTimeout(reject, timeout);
     
-     fetch(request)
+     return fetch(request)
        .then(function (response) {
         console.log('in fetch');
      //     clearTimeout(timeoutId);
