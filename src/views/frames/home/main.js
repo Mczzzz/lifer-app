5ef391724f52baa7d435aadf7d26e1a93ae908a2
Guillaume Bailly <g.bailly@immanens.com>
2019-01-30 00:09:31 +0100

    MAJ

--- src/views/frames/home/main.js
+++ src/views/frames/home/main.js
@@ -1,8 +1,7 @@
 import superViews from "../../common/superViews.js";
 
-import Card from "../../common/ui/card.js";
-
-import LoaderCollection from '../../../services/Loader/LoaderCollection.js';
+import UniversButton from "./main/UniversButtons.js";
+import NoteList from "./main/NoteList.js";
 
 export default class Main extends superViews{
 	
@@ -21,109 +20,27 @@ export default class Main extends superViews{
 		//this.setStyle("background" , "linear-gradient(45deg, rgb(199, 28, 28) 0%, rgb(216, 216, 216) 100%)");
 		this.setStyle("background" , "white");
 		this.setStyle("flex" , 1);
-		this.setStyle("alignItems" , "center");
-		this.setStyle("overflowY" , "scroll");
+		this.setStyle("display", "flex");
 
-		this.NoteCollection = new LoaderCollection('Notes');
-		//je récupère la liste des notes
-		//j'affiche
-		this.showList();
-		this.service = setInterval(()=> this.showList() , 10000 );
+		this.initChilds();
+
+
+	}
+
+
+
+
+	initChilds(){
+
+		this.UniversButton = new UniversButton("UniversButton",this.path);
+		this.NoteList = new NoteList("NoteList",this.path);
+
+	}
 	
 		
-	}
 
 
 
-
-	showList(){
-
-
-		this.NoteCollection.getAllNotes(this,'addCards');
-
-
-	}
-
-	addCards(datas){
-
-
-		
-		let len = datas.rows.length, i;
-		  for (i = 0; i < len; i++) {
-
-
-		  	this.createCard(datas.rows[i]);
-
-
-		  }
-
-
-
-
-
-	}
-
-
-	createCard(datas){
-
-		let id = datas.note_id;
-
-			let card = new Card('Card'+id, this.path);
-	
-		    card.setStyle("borderWidth", "1px");
-		    card.setStyle("borderRadius", "3px");
-		    card.setStyle("margin", "5px");
-
-
-		    let bkgColor = (datas.status == "SYNC")? "lightsteelblue" : "navajowhite";
-		    card.setStyle("background", bkgColor);
-
-		    card.getContainer().addEventListener("click",()=>this.openNote(id));	
-
-
-			let Elt2 = card.setElement("Element2"+id);
-			Elt2.setStyle("justifyContent","flex-end");
-
-
-
-			let date = this.Moment(datas.timestamp).fromNow();
-			let 	itemDate = card.push("TextButton", Elt2,"date_Note"+id,date);
-			itemDate.setStyle('font-size', '10px');
-			itemDate.setStyle("padding", "3px");
-			itemDate.setStyle("alignItems", "baseline");
-
-
-
-
-			let Elt = card.setElement("Element"+id);
-			Elt.setStyle("justifyContent","flex-start");
-
-
-			
-			let 	item = card.push("TextButton", Elt,"view_Note"+id,datas.note_title);
-			item.setStyle("padding", "2px 0px 10px 10px");
-				
-
-
-
-		
-
-
-	}
-
-
-
-	openNote(guid){
-
-	 let LinkEvent = new CustomEvent('changeRoute', {'detail' : {'frame' : 'Note',
-	 	                                                          'guid' : guid
-	 	                                                         }
-	 	                                             }
-	 	                            );
-     window.dispatchEvent(LinkEvent);
-
-
-	}
 
 
 
