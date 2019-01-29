c205f3a837ef304c2242a8c0fe35b9e116338767
Guillaume Bailly <g.bailly@immanens.com>
2019-01-29 13:18:59 +0100

    Mise  ajour taille Image

--- src/views/frames/note/main/story.js
+++ src/views/frames/note/main/story.js
@@ -399,6 +399,7 @@ export default class Story extends superViews{
 				MyLegend.setStyle("margin","5px 0px 0px 5px");
 				MyLegend.setStyle("borderLeft","1px solid black");
 				MyLegend.setStyle("padding","0px 0px 5px 10px");
+				MyLegend.setData(datas.item_text);
 				this.RessourceList[RessourcePathId].Items[ItemPathId].object = MyLegend;
 
 /*				let MyLegendconfig = { attributes: true, characterData: true, childList: true, subtree: true};
