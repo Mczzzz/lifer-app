1159cea0d737c0ffb9aa1c16583ab5095f2b1e84
g.bailly <g.bailly@immanens.coms>
2019-01-30 23:58:53 +0100

    debug login qui se reload 2 fois

--- src/views/frames/home/main/UniversButtons.js
+++ src/views/frames/home/main/UniversButtons.js
@@ -35,6 +35,14 @@ export default class UniversButtons extends superViews{
 
 	showButtons(){
 
+		//1F4A1
+		let idea = this.card.push("Button",this.Elt,"ideaButton", String.fromCodePoint(0x1F4A1));
+        idea.setStylePicto("fontSize","50px");
+		idea.setStylePicto("border", "1px solid black");
+        idea.setStylePicto("padding","10px 10px 20px 10px");
+        idea.setStylePicto("borderRadius" ,"10px");
+
+
 		let object = this.card.push("Button",this.Elt,"objectButton", String.fromCodePoint(0x1F4E6));
         object.setStylePicto("fontSize","50px");
 		object.setStylePicto("border", "1px solid black");
