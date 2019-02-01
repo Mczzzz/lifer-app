8232d66aba7ee400ba42b8ef34248b931bc89a0c
Guillaume Bailly <g.bailly@immanens.com>
2019-02-01 21:06:47 +0100

    design add

--- src/views/frames/timer/add/add.js
+++ src/views/frames/timer/add/add.js
@@ -76,7 +76,7 @@ export default class Add extends superViews{
 			this.MainElement.setStyle("flex",1);
 
 
-				this.LogoTap = this.card.push("Text", this.MainElement,"TapLogo", String.fromCodePoint(0x2753)); //274C
+				this.LogoTap = this.card.push("Text", this.MainElement,"TapLogo", ""); //274C
 				this.LogoTap.setAttribute("placeholder",String.fromCodePoint(0x2753));
 				this.LogoTap.setStyle("color", "black");
 				this.LogoTap.setStyle("fontSize", "100px");
