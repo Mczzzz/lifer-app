de94811defd9d08dc4270a084dcd64470b7be39f
g.bailly <g.bailly@immanens.coms>
2019-02-04 18:07:38 +0100

    Mise a jour graphiques

--- src/collections/TapCollection.js
+++ src/collections/TapCollection.js
@@ -55,7 +55,7 @@ export default class TapCollection {
 	getEventsTapersTodayByHour(id,callBackObj,callBackMethod){
 
 		//let qry = "SELECT * FROM TapsEvents WHERE tapId = '"+id+"'  AND timestamp > strftime('%Y-%m-%d', 'now') ORDER BY timestamp ASC";
-		let qry = "SELECT *,strftime('%H', timestamp) as valHour, SUM(increment) AS incr FROM TapsEvents WHERE tapId = '"+id+"' AND timestamp > strftime('%Y-%m-%d', 'now') GROUP BY valHour 
+		let qry = "SELECT *,strftime('%H', timestamp) as valHour, SUM(increment) AS incr FROM TapsEvents WHERE tapId = '"+id+"' AND timestamp > strftime('%Y-%m-%d', 'now') GROUP BY valHour"; 
 		// je copie dans ma base de remonté syncUP les LOCAL de plus d'une seconde
 		this.webSQL.playQuery('cacheData',qry,callBackObj,callBackMethod);
 
