9ae22a6903f02aa8d22f0cae99bf57b91351736c
g.bailly <g.bailly@immanens.coms>
2019-02-05 15:52:10 +0100

    Mise a jour graphiques

--- src/services/IO/fetcher.js
+++ src/services/IO/fetcher.js
@@ -6,7 +6,8 @@ export default class Fetcher {
 
 
 
-ajaxSend(VERB,url, dataSend = false){
+//	ajaxSend(VERB,url,dispatchResponseTo=false,dataCallback = false,dataSend = false){
+	ajaxSend(VERB,url,dataSend = false){
 //		//console.log"on passe bien dans ajaxSend");		
 			let MyResponse = false;
 
@@ -20,10 +21,79 @@ ajaxSend(VERB,url, dataSend = false){
 
 //			//console.log'JUST BEFORE SEND FETCH');
 //TODO on verifie qu'on est considéré comme loggé
-			let resp = fetch(host+url, params);
+			fetch(host+url, params)
+			  .then(function(response) {
 
-			return resp;
-			
+			  	//MyResponse = response.clone(); 
+				//console.log'in my fetch app :');
+			//	//console.logresponse.pro)
+			////console.loga);
+				if(response.status === 401) {
+					//console.logresponse.status);
+				let qry = "INSERT INTO Params (name, value) VALUES ('is_auth', false)";
+				webSQLCon.playQuery('cacheData',qry);
+//TODO : il faut flagger por dire que la seule requete qui passera sera celle de login
+
+					//il faut te logger monsieur
+					let LinkEvent = new CustomEvent('changeRoute', {'detail' : {'frame' : 'Login'}});
+					window.dispatchEvent(LinkEvent);
+
+					//informé l'envoyeur que sa requete n'a pas été faite (donc retentative à prevoir)
+					//return false;
+
+				}else{
+					console.log(response);
+
+					let contentType = response.headers.get("content-type");
+
+					if(contentType && contentType.indexOf("application/json") !== -1) {
+
+						response.json().then(function(jsonResp){
+							//console.log'in responpose json');
+							//console.logjsonResp);
+							//console.logcallBackObj);
+							//console.logcallBackMethod);
+							//console.logtypeof callBackObj);
+							//console.logcallBackMethod.length);
+							if(typeof callBackObj === "object" && callBackMethod.length > 0 ){
+								//console.logcallBackObj);
+								//console.logcallBackMethod);
+								callBackObj[callBackMethod](jsonResp);
+
+							} 
+							//console.log'before return json');
+							return jsonResp;
+						});
+					  
+					}else{
+
+					  
+						response.blob().then(function(blobResp){
+							//console.log'in responpose blob');
+							if(typeof callBackObj === "object" && callBackMethod.length > 0 ){
+
+								callBackObj[callBackMethod](blobResp);
+
+							} 
+							//console.log'before return blob');
+							return blobResp;
+					  
+						});
+
+
+					
+
+
+					}
+
+				}
+
+
+			}).catch(function(error){
+
+				//console.log"in catch of fectch "+error);
+
+			});
 
 
 
